dd0c251ac491f387482f71ad8c9664f51b27a608
luizneves <luiz.neves9@gmail.com>
2026-07-09 22:03:37 -0300

    Atualizações na estrutura.

--- src/app.py
+++ src/app.py
@@ -1,14 +1,17 @@
-import os
-import pandas as pd
-import hashlib
 from sql import conectar_banco
 import streamlit as st
-from sqlalchemy import text
-from datetime import date
 import warnings
-import uuid
-from dotenv import load_dotenv
 from security.ip_validator import verificar_acesso_ip
+
+# =========================================================================
+# CONFIGURAÇÃO INICIAL
+# =========================================================================
+
+st.set_page_config(
+    page_title='NEXUS',
+    layout='wide',
+    initial_sidebar_state='expanded'
+    )
 
 # =========================================================================
 # VALIDAÇÃO DE ACESSO - IP
@@ -22,28 +25,23 @@ if not verificar_acesso_ip():
 # DESENVOLVIMENTO
 # =========================================================================
 
-## configurações iniciais
-
 # configuração para visualização no terminal
-pd.set_option('display.max_columns', None)
 warnings.filterwarnings('ignore', category=DeprecationWarning)
 
 # conectando engine
 engine = conectar_banco()
 
-# streamlit pagina
-st.set_page_config(
-    page_title='NEXUS',
-    layout='wide',
-    initial_sidebar_state='expanded'
-    )
+
 
 def main():
 
+    pagina_extrato = st.Page('pages/extrato_bancario.py', title='Extrato Bancário', default=True)
+    pagina_liquidacoes = st.Page('pages/extrato_liquidacoes.py', title='Liquidações')
+
     pages = {
         'Menu:': [
-            st.Page('pages/extrato_bancario.py', title='Extrato Bancário'),
-            st.Page('pages/extrato_liquidacoes.py', title='Liquidações')
+            pagina_extrato,
+            pagina_liquidacoes
         ]
     }
 
