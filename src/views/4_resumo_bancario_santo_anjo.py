236ad0ae9ba74c7bd708db69cd83b6813d28efa5
Luiz <luiz.neves9@gmail.com>
2026-07-23 11:46:55 -0300

    Atualização da aba de extrato bancário da santo anjo.

--- src/views/4_resumo_bancario_santo_anjo.py
+++ src/views/4_resumo_bancario_santo_anjo.py
@@ -19,7 +19,7 @@ def main():
 
     df_extrato_santo_anjo = pd.read_sql(SELECT_EXTRATO_SANTO_ANJO, engine)
 
-    colunas_valor = ['vlr_anjo', 'vlr_desconhecido', 'vlr_bsr']
+    colunas_valor = ['valor_banco', 'vlr_anjo', 'vlr_desconhecido', 'vlr_bsr']
     for col in colunas_valor:
         df_extrato_santo_anjo[col] = df_extrato_santo_anjo[col].map(transformar_valor_decimal_em_str)
 
@@ -28,8 +28,19 @@ def main():
         hide_index=True,
         width='stretch',
         column_config={
-            'id': st.column_config.NumberColumn('ID', format='%d'),
-            'data_contabil': st.column_config.DateColumn('Data Extrato', format='DD/MM/YYYY'),
+            'id': st.column_config.NumberColumn('Id', format='%d'),
+            'nome_empresa': st.column_config.TextColumn('Empresa'),
+            'banco': st.column_config.TextColumn('Banco'),
+            'agencia_conta': st.column_config.TextColumn('Ag/Cc'),
+            'data_contabil': st.column_config.DateColumn('Data', format='DD/MM/YYYY'),
+            'descricao_historico': st.column_config.TextColumn('Hist.'),
+            'documento': st.column_config.TextColumn('Doc.'),
+            'complemento': st.column_config.TextColumn('Comp.'),
+            'tipo': st.column_config.TextColumn('Tipo'),
+            'valor_banco': st.column_config.TextColumn('Vlr Banco'),
+            'vlr_anjo': st.column_config.TextColumn('Vlr Anjo'),
+            'vlr_desconhecido': st.column_config.TextColumn('Vlr Desc.'),
+            'vlr_bsr': st.column_config.TextColumn('Vlr Bsr'),
         },
     )
 
